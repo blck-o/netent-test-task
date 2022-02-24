f6550104a26bdab15639b105f3bd6fedfae9c9c2
Livio Agolini <fallena238@gmail.com>
2022-02-24 18:39:36 +0200

    enhances

--- framework/src/main/java/com/netent/games/simplegames/framework/game/AbstractInteractiveGame.java
+++ framework/src/main/java/com/netent/games/simplegames/framework/game/AbstractInteractiveGame.java
@@ -64,4 +64,3 @@ public abstract class AbstractInteractiveGame extends AbstractGame {
     }
 
 }
-
--- framework/src/main/java/com/netent/games/simplegames/framework/game/InteractiveGame.java
+++ framework/src/main/java/com/netent/games/simplegames/framework/game/InteractiveGame.java
@@ -2,7 +2,6 @@ package com.netent.games.simplegames.framework.game;
 
 public interface InteractiveGame extends Game {
 
-    // TODO interactive cli game
     void cli();
 
 }
